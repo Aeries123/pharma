fcca03679ad4d926948c3d284b6c03f78061c6f2
Aeries123 <111567095+Aeries123@users.noreply.github.com>
2023-11-18 22:33:00 +0530

    Update db_connection.php

--- php/db_connection.php
+++ php/db_connection.php
@@ -1,16 +1,25 @@
 <?php
-  $SERVER = 'localhost';
-  $USERNAME = 'root';
-  $PASSWORD = '';
-  $DB = 'pharmacy';
+  $HOST = getenv('MYSQL_HOST');
+  $USERNAME = getenv('MYSQL_NAME');
+  $PASSWORD = getenv('MYSQL_PASS');
+  $DB = getenv('MYSQL_DB');
 
-  @$con = mysqli_connect($SERVER, $USERNAME, $PASSWORD, $DB)
+  #function __construct()
+  #{
+   # $this->HOST = getenv('MYSQL_HOST');
+    #$this->DB = getenv('MYSQL_DB');
+    #$this->USERNAME = getenv('MYSQL_NAME');
+    #$this->PASSWORD = getenv('MYSQL_PASS');
+  #}
+  
+
+  @$con = mysqli_connect('127.0.0.1', $USERNAME, $PASSWORD, $DB)
   or
   die("<div class='text-danger text-center h5'>Oops, Unable to connect with database!</div>");
 
   if(isset($_GET['action']) && $_GET['action'] == 'is_logged_in') {
     $query = "SELECT IS_LOGGED_IN FROM admin_credentials";
-    $result = mysqli_query($con, $query);
+    $result = mysqli_query($con,$query);
     if($result) {
       $row = mysqli_fetch_array($result);
       echo $row['IS_LOGGED_IN'];
